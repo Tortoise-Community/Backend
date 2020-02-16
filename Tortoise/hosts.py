159158e84642b1d79cbc685dfd455c0bedce19aa
hideki_ryuga <vaishnav2353@gmail.com>
2020-02-16 22:15:17 +0530

    final

--- Tortoise/hosts.py
+++ Tortoise/hosts.py
@@ -4,5 +4,5 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     host(r'www', 'Tortoise.urls', name='www'),
-    host(r'(?!www).*', 'userdata.urls', name='api'),
+    host(r'api', 'userdata.urls', name='api'),
 )
