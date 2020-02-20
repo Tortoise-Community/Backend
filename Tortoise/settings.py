7ce5f6df9d7090e4298e9f9ae4b45cc8f3ec5fe1
Vaishnav <vaishnavmg1327@gmail.com>
2020-02-20 21:26:09 +0530

    added admin-site css

--- Tortoise/settings.py
+++ Tortoise/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'assets')]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
