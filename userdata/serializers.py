e922a23707b3767d68d33dcfeb87264a3742a83b
Vaishnav <vaishnavmg1327@gmail.com>
2020-03-25 22:56:35 +0530

    MemberSerializer updated

--- userdata/serializers.py
+++ userdata/serializers.py
@@ -5,7 +5,7 @@ from . import views
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model=Members
-        fields = ['user_id','guild_id','join_date']
+        fields = ['user_id','guild_id','join_date','member','name','tag']
 
 
 class AllSerializer(serializers.ModelSerializer):
