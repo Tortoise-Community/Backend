a1052e9374abcc3938225257ee791c3895ea8729
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-04-09 23:56:21 +0530

    AllSerializer excludes email

--- userdata/serializers.py
+++ userdata/serializers.py
@@ -11,7 +11,6 @@ class MemberRoleSerializer(serializers.ModelSerializer):
 class AllSerializer(serializers.ModelSerializer):
     class Meta:
         model = Members
-        fields ='__all__'    
         exclude = ('email',) 
 
 class GithubSerializer(serializers.ModelSerializer):
