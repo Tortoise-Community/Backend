3e3a7c879b00b6fd8559ed8d4139b7394d96ead2
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-04-10 23:10:27 +0530

    cdn links added

--- Tortoise/models.py
+++ Tortoise/models.py
@@ -4,12 +4,20 @@ from django.db import models
 
 class SiteUrls():
     verificationimg = "https://cdn.discordapp.com/attachments/649868379372388352/698159203696640041/verification.jpg"
+    #Index
+    recent_event_img = "https://cdn.discordapp.com/attachments/649868379372388352/698225354653040722/fire.png"
+    live_event_img = "https://cdn.discordapp.com/attachments/649868379372388352/698214532899930122/ilovecoding.png"
+    #Page top bg
     img1 = "https://cdn.discordapp.com/attachments/649868379372388352/698159424904101918/1.jpg"
     img2 = "https://cdn.discordapp.com/attachments/649868379372388352/698160457185230929/7.jpg"
     img3 = "https://cdn.discordapp.com/attachments/649868379372388352/698159665199972452/3.jpg"
     img4 = "https://cdn.discordapp.com/attachments/649868379372388352/698159533066944592/4.jpg"
     img5 = "https://cdn.discordapp.com/attachments/649868379372388352/698159667863617596/5.jpg"
     img6 = "https://cdn.discordapp.com/attachments/649868379372388352/698159905281933342/6.jpg"
+    #Footer
+    footerimg = "https://cdn.discordapp.com/attachments/649868379372388352/698214484505788556/footer-top-bg.png"
+    footerlogo = "https://cdn.discordapp.com/attachments/649868379372388352/698225413457182870/footer-logo.png"
+    #Social links
     github = "https://github.com/Tortoise-Community"
     twitter = "https://twitter.com/PythonTortoise"
     reddit = "https://www.reddit.com/user/Tortoise_Community"
