6985aacde2999a41cda0732a542b984648a0a9e5
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-04-11 17:03:52 +0530

    site optimized

--- Tortoise/models.py
+++ Tortoise/models.py
@@ -7,15 +7,15 @@ class SiteUrls():
     verificationimg = "https://cdn.discordapp.com/attachments/649868379372388352/698159203696640041/verification.jpg"
     site_logo = "https://cdn.discordapp.com/attachments/649868379372388352/698236524872859748/logo.png"
     #Index
-    recent_event_img = "https://cdn.discordapp.com/attachments/649868379372388352/698225354653040722/fire.png"
-    live_event_img = "https://cdn.discordapp.com/attachments/649868379372388352/698214532899930122/ilovecoding.png"
+    recent_event_img = "https://cdn.discordapp.com/attachments/649868379372388352/698290351978578050/fire.jpg"
+    live_event_img = "https://cdn.discordapp.com/attachments/649868379372388352/698290304859504671/ilovecoding.jpg"
     #Page top bg
-    img1 = "https://cdn.discordapp.com/attachments/649868379372388352/698159424904101918/1.jpg"
-    img2 = "https://cdn.discordapp.com/attachments/649868379372388352/698160457185230929/7.jpg"
-    img3 = "https://cdn.discordapp.com/attachments/649868379372388352/698159665199972452/3.jpg"
-    img4 = "https://cdn.discordapp.com/attachments/649868379372388352/698159533066944592/4.jpg"
-    img5 = "https://cdn.discordapp.com/attachments/649868379372388352/698159667863617596/5.jpg"
-    img6 = "https://cdn.discordapp.com/attachments/649868379372388352/698159905281933342/6.jpg"
+    img1 = "https://cdn.discordapp.com/attachments/649868379372388352/698290168494424124/2.jpg"
+    img2 = "https://cdn.discordapp.com/attachments/649868379372388352/698290356969537536/7.jpg"
+    img3 = "https://cdn.discordapp.com/attachments/649868379372388352/698290244985815110/3.jpg"
+    img4 = "https://cdn.discordapp.com/attachments/649868379372388352/698290171019526194/4.jpg"
+    img5 = "https://cdn.discordapp.com/attachments/649868379372388352/698290211658006579/5.jpg"
+    img6 = "https://cdn.discordapp.com/attachments/649868379372388352/698290295678304256/6.jpg"
     #Footer
     footerimg = "https://cdn.discordapp.com/attachments/649868379372388352/698214484505788556/footer-top-bg.png"
     footerlogo = "https://cdn.discordapp.com/attachments/649868379372388352/698225413457182870/footer-logo.png"
@@ -25,3 +25,4 @@ class SiteUrls():
     reddit = "https://www.reddit.com/user/Tortoise_Community"
     facebook = "https://www.facebook.com/Tortoise-Python-Community-103144314517830"
     instagram = "https://www.instagram.com/Tortoise_Python_Community/"
+    
