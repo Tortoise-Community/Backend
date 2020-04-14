6624d209b8c7c047318d1b75ff105394bde090e8
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-04-14 18:38:49 +0530

    Authentication turned off

--- userdata/views.py
+++ userdata/views.py
@@ -14,8 +14,9 @@ from rest_framework.decorators import api_view
 
 @csrf_exempt
 @api_view(['GET'])
+@permission_classes([])
 def ping(request):
-	return JsonResponse(status=200,safe=False)
+	return HttpResponse(status=200,safe=False)
 
 
 @csrf_exempt
