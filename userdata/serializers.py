7f84602d4bd09f1a7cec1fc5b6bb1d7481b14cfa
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-04-14 23:43:22 +0530

    error fix in views 1

--- userdata/serializers.py
+++ userdata/serializers.py
@@ -51,4 +51,4 @@ class StatusSerializer(serializers.ModelSerializer):
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServerUtils
-        fields = ['Github_Microservice','Status_Micorservice','Tortoise_BOT','Tortoise_BOT2','Website','Sockets']                
+        fields = ['Github_Microservice','Status_Microservice','Tortoise_BOT','Tortoise_BOT2','Website','Sockets']                
