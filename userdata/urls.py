a509f1836ea53eebff9e04bb02115e8f1ba24d39
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-04-18 02:49:58 +0530

    new endpoint for editing developers model

--- userdata/urls.py
+++ userdata/urls.py
@@ -15,12 +15,12 @@ path('private/projects/',views.projects),
 path('private/services/<int:id>/',views.get_services),
 path('private/bot/status/<int:id>/',views.manage_bot_status),
 path('private/top-members/',views.get_top_members),
-#path('private/top-members/<int:id>/',views.put_top_members),
+path('private/developers/edit/<int:id>/',views.put_top_developers),
 path('private/auth/', include('rest_framework.urls')),
 path('private/members/edit/<int:id>/',views.members_edit),
 path('private/projects/edit/<int:pk>/',views.projects_edit),
 path('private/members/<int:id>/roles/',views.get_member_roles),
 path('private/verify-confirmation/<int:id>/',views.is_verified),
-path('private/developers/',views.top_members)
+path('private/developers/',views.developers)
 
 ]
