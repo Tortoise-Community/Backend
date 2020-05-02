a99df231a6a3a20e397f550c8e27c921b5339ef0
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-05-03 01:59:57 +0530

    test push

--- userdata/models.py
+++ userdata/models.py
@@ -64,7 +64,6 @@ class Rules(models.Model):
     
 class ServerUtils(models.Model):
 	guild_id = models.BigIntegerField(primary_key=True)
-    suggestion_msg_id = models.BigIntegerField(null=True,blank=True)
 	event_submission = models.BooleanField(null=True,default=False)
 	bug_report = models.BooleanField(null=True,default=False)
 	mod_mail = models.BooleanField(null=True,default=False)
@@ -76,6 +75,7 @@ class ServerUtils(models.Model):
 	Website = JSONField(null=True,default=default_json)
 	Sockets = JSONField(null=True,default=default_json)
 	API_Gateway = JSONField(null=True,default=default_json)
+    suggestion_msg_id = models.BigIntegerField(null=True,blank=True)
 
  
 class Developers(models.Model):
