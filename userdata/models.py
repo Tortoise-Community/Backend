a8a4c7e4d39999cefc99312c19edc25858821cb5
hideki-ryuga <vaishnavmg1327@gmail.com>
2020-05-03 01:57:59 +0530

    test push

--- userdata/models.py
+++ userdata/models.py
@@ -64,10 +64,10 @@ class Rules(models.Model):
     
 class ServerUtils(models.Model):
 	guild_id = models.BigIntegerField(primary_key=True)
+    suggestion_msg_id = models.BigIntegerField(null=True,blank=True)
 	event_submission = models.BooleanField(null=True,default=False)
 	bug_report = models.BooleanField(null=True,default=False)
 	mod_mail = models.BooleanField(null=True,default=False)
-    #suggestion_msg_id = models.BigIntegerField(null=True,blank=True)    
 	bot_status = JSONField(null=True,default=default_status,blank=True) 
 	Github_Microservice = JSONField(null=True,default=default_json)
 	Status_Microservice = JSONField(null=True,default=default_json)
