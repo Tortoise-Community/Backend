65094cf750a2c198eefd3c760eb71004a4586509
Ryuga <vaishnav2353@gmail.com>
2020-06-25 20:25:23 +0530

    updated the oauth2 urls

--- utils/oauth.py
+++ utils/oauth.py
@@ -1,15 +1,16 @@
 import requests
 from django.conf import settings
 
+
 class Oauth(object):
     client_id = settings.OAUTH_CLIENT_ID
     client_secret = settings.OAUTH_CLIENT_SECRET
     scope  = "identify%20email"
     redirect_uri = "https://www.tortoisecommunity.ml/verification/"
-    discord_login_url = "https://discordapp.com/api/oauth2/authorize?client_id={}&" \
+    discord_login_url = "https://discord.com/api/oauth2/authorize?client_id={}&" \
                         "redirect_uri={}&response_type=code&scope={}".format(client_id,redirect_uri,scope)
-    discord_token_url = "https://discordapp.com/api/oauth2/token"
-    discord_api_url = "https://discordapp.com/api"
+    discord_token_url = "https://discord.com/api/oauth2/token"
+    discord_api_url = "https://discord.com/api"
 
     @staticmethod
     def get_access_token(code):
@@ -29,7 +30,6 @@ class Oauth(object):
         access_token = requests.post(url=Oauth.discord_token_url,data=payload,headers=headers)
         json = access_token.json()
         return json.get("access_token")
-        
 
     @staticmethod
     def get_user_json(access_token):
