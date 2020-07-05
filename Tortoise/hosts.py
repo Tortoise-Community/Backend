ce07fc516f148bc0138a066176f9845e5f5a54a4
Ryuga <vaishnav2353@gmail.com>
2020-07-05 16:03:10 +0530

    Root domain test

--- Tortoise/hosts.py
+++ Tortoise/hosts.py
@@ -5,5 +5,6 @@ host_patterns = patterns(
     '',
     host(r'www', 'websitedata.urls', name='www'),
     host(r'api', 'userdata.urls', name='api'),
-    host(r'staff', 'Tortoise.admin_urls', name='staff'),
+    host(r'staff', 'Tortoise.urls', name='staff'),
+    host(r'', 'websitedata.urls', name=''),
 )
--- Tortoise/urls.py
+++ Tortoise/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path
 
-urlpatterns = [
 
-    # path('', include('websitedata.urls'))
-
-]
+urlpatterns = (
+    path('', admin.site.urls),
+)
