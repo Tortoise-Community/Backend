d958e12403f500359a6516d12ac1f7f262a8eb41
Ryuga <vaishnav2353@gmail.com>
2020-07-07 22:13:46 +0530

    url fix

--- Tortoise/hosts.py
+++ Tortoise/hosts.py
@@ -6,5 +6,4 @@ host_patterns = patterns(
     host(r'www', 'websitedata.urls', name='www'),
     host(r'api', 'userdata.urls', name='api'),
     host(r'staff', 'Tortoise.urls', name='staff'),
-    host(r'', 'websitedata.urls', name=''),
 )
