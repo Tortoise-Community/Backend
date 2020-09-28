cc89e4a1a8e91d4141ae11bdfdd668a19ce57f04
ryuga <vaishnav2353@gmail.com>
2020-09-29 01:35:04 +0530

    refactor misc

--- userdata/admin.py
+++ userdata/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Member, Projects, Rules, Suggestions
+from .models import Member, Projects, Rules, Suggestions, User, Guild, Role
 
 # Register your models here.
 admin.site.register(Member)
 admin.site.register(Projects)
 admin.site.register(Rules)
 admin.site.register(Suggestions)
+admin.site.register(User)
+admin.site.register(Guild)
+admin.site.register(Role)
--- userdata/urls.py
+++ userdata/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
-from .serializers import TopMemberSerializer, MemberMetaSerializer, MemberModSerializer
 from .views import (
-    DynamicMemberView, SuggestionDataView, MemberDataView, GuildDataView,
+    UserDataView, SuggestionDataView, MemberDataView, GuildDataView,
     ProjectStatsView, RulesDataView
 )
 
@@ -13,18 +12,18 @@ urlpatterns = [
     path('private/bot/', include(router.urls)),
     path('private/auth/', include('rest_framework.urls')),
 
-    path('private/suggestions/', SuggestionDataView.as_view()),
-    path('private/suggestions/<int:item_id>/', SuggestionDataView.as_view()),
+    path('private/suggestions/<int:guild_id>/', SuggestionDataView.as_view()),
+    path('private/suggestions/item/<int:item_id>/', SuggestionDataView.as_view()),
 
-    path('private/rules/<int:item_id>/', RulesDataView.as_view()),
-    path('private/guild/<int:item_id>/', GuildDataView.as_view()),
+    path('private/rules/<int:guild_id>/', RulesDataView.as_view()),
+    path('private/guild/', GuildDataView.as_view()),
+    path('private/guild/<int:guild_id>/', GuildDataView.as_view()),
 
     path('private/projects/', ProjectStatsView.as_view()),
     path('private/projects/<int:item_id>/', ProjectStatsView.as_view()),
 
     path('private/members/', MemberDataView.as_view()),
-    path('private/members/<int:item_id>/', MemberDataView.as_view()),
-    path('private/members/top/', DynamicMemberView.as_view(serializers=TopMemberSerializer)),
-    path('private/members/meta/<int:item_id>/', DynamicMemberView.as_view(serializers=MemberMetaSerializer)),
-    path('private/members/moderation/<int:item_id>/', DynamicMemberView.as_view(serializers=MemberModSerializer)),
+    path('private/members/<int:user_id>/<int:guild_id>/', MemberDataView.as_view()),
+    path('private/user/<int:user_id>/', UserDataView.as_view()),
+    path('private/user/', UserDataView.as_view()),
 ]
