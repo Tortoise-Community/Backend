8fea422e83dcfafdb910db230ce9ea130da9f21f
ryuga <vaishnav2353@gmail.com>
2020-10-07 14:10:06 +0530

    Add dashboard panel

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+class LoginView(View):
+    template_name = "dashboard/accounts/login.html"
+    context = {}
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class PanelView(View):
+    template_name = "dashboard/index.html"
+    context = {}
+
+    def get(self, request):
+        return render(request, self.template_name)
