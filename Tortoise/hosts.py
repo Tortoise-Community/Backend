4ca9b79dbefd63d3fb57801a814adeb0ff56963c
ryuga <vaishnav2353@gmail.com>
2020-10-07 10:45:13 +0530

    add urls

--- Tortoise/hosts.py
+++ Tortoise/hosts.py
@@ -6,4 +6,5 @@ host_patterns = patterns(
     host(r'www', 'websitedata.urls', name='www'),
     host(r'api', 'userdata.urls', name='api'),
     host(r'staff', 'Tortoise.urls', name='staff'),
+    host(r'dashboard', 'admin_dashboard.urls', name='dashboard'),
 )
