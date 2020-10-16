eeee8aa27ca76df360177cdad9378a6a5006528d
ryuga <vaishnav2353@gmail.com>
2020-10-17 02:36:43 +0530

    Authentication working model

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login, authenticate
 from utils.oauth import Oauth
 from utils.encryption import Encryption
-from userdata.models import Guild
-from utils.operations import create_or_update_admin
+from userdata.models import Guild, Admins
+from utils.operations import create_admin, update_guilds, get_admin_guild_list
 oauth = Oauth(redirect_uri="http://dashboard.tortoisecommunity.co:8000/login/", scope="guilds%20identify%20email")
 encryption = Encryption()
 
@@ -28,32 +28,19 @@ class LoginView(View):
             self.email = self.user_json.get('email')
         if self.user_id and self.email is not None:
             password = encryption.encrypted_user_pass(self.user_id, self.email)
+            guilds = oauth.get_guild_info_json(self.access_token)
+            admin_guilds = get_admin_guild_list(guilds)
+            if len(admin_guilds) == 0:
+                return  # TODO: DELETE EXISTING CREDENTIALS AND SEND MESSAGE
             user = authenticate(username=self.user_id, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("/login")
+                admin_user = Admins.objects.get(user_id=self.user_id)
+                update_guilds(admin_user, admin_guilds)
             else:
-                guilds = oauth.get_guild_info_json(self.access_token)
-                tortoise_guilds = Guild.get_id_list()
-                admin_guilds = []
-                for guild in guilds:
-                    if guild["id"] in tortoise_guilds:
-                        if int(guild["permissons"])&8:
-                            admin_guilds.append(guild)
-                if admin_guilds is not None:
-                    create_or_update_admin(user_json=self.user_json, admin_guilds=admin_guilds, password=password)
-
+                create_admin(user_json=self.user_json, admin_guilds=admin_guilds, password=password)
+        # TODO: IMPLEMENT AUTHENTICATION
         return render(request, self.template_name, {"Oauth": oauth})
-    #
-    # def post(self, request):
-    #     form = LoginForm(request.POST or None)
-    #     if form.is_valid():
-    #         username = form.cleaned_data.get("username")
-    #         password = form.cleaned_data.get("password")
-    #         user = authenticate(username=username, password=password)
-    #         if user is not None:
-    #             login(request, user)
-    #             return redirect("/panel")
 
 
 class GuildPanelView(View):
--- utils/operations.py
+++ utils/operations.py
@@ -1,13 +1,31 @@
 from django.contrib.auth.models import User as AuthUser
-from userdata.models import Admins, User as MUser, Guild
+from userdata.models import Admins, Guild
 
 
-def create_or_update_admin(user_json: dict, admin_guilds: list, password: str):
+def get_admin_guild_list(guilds):
+    tortoise_guilds = Guild.get_id_list()
+    admin_guilds = []
+    for guild in guilds:
+        if int(guild["id"]) in tortoise_guilds:
+            if int(guild["permissions"]) & 8:
+                admin_guilds.append(guild["id"])
+    return admin_guilds
+
+
+def update_guilds(instance: Admins, guild_list: list):
+    if instance.guild is not None:
+        instance.guild.clear()
+    for guild in guild_list:
+        instance.guild.add(guild)
+    instance.save()
+
+
+def create_admin(user_json: dict, admin_guilds: list, password: str):
     auth_user = AuthUser.objects.create_user(username=user_json["id"],
                                              password=password,
                                              is_staff=True,
                                              first_name=user_json["username"],
                                              email=user_json["email"]
                                              )
-    user = MUser.objects.get(id=user_json["id"])
-    Admins.objects.create(authuser=auth_user, user=user)
+    admin_user = Admins.objects.create(authuser=auth_user, user_id=user_json["id"])
+    update_guilds(admin_user, admin_guilds)
