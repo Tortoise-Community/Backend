14f56205a8c33ee74ab661f299bb58c89167a1c3
ryuga <vaishnav2353@gmail.com>
2021-11-03 22:47:06 +0530

    Update pre production alpha

--- core/hosts.py
+++ core/hosts.py
@@ -3,8 +3,8 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    host(r'www', 'web.urls', name='www'),
-    host(r'api', 'api.urls', name='api'),
+    host(r'web', 'web.urls', name='www'),
+    # host(r'api', 'api.urls', name='api'),
     host(r'staff', 'core.urls', name='staff'),
-    host(r'dash', 'dash.urls', name='dashboard'),
+    # host(r'dash', 'dash.urls', name='dashboard'),
 )
