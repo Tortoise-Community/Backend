61411fbb9f041ee49e76847053be3e6db86ca137
ryuga <vaishnav2353@gmail.com>
2021-11-04 18:58:56 +0530

    Enable CORS

--- core/settings.py
+++ core/settings.py
@@ -15,15 +15,13 @@ if DEBUG:
 else:
     ALLOWED_HOSTS = ['.tortoisecommunity.org']
 
-# CORS_ALLOWED_ORIGINS = [
-    # "https://tortoisecommunity.com",
-    # "https://www.tortoisecommunity.org",
-    # "http://localhost:8080",
-    # "http://127.0.0.1:9000"
-    # "*"
-# ]
-
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://tortoisecommunity.com",
+    "https://www.tortoisecommunity.org",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000"
+]
 
 DATE_INPUT_FORMATS = ['%Y-%m-%d']
 
