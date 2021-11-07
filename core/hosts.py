3e92a60711a0754136fb403e0f39125c32d56e65
Doctor <thirvondukr@gmail.com>
2021-11-07 12:52:06 +0300

    Fix web urls

--- core/hosts.py
+++ core/hosts.py
@@ -3,7 +3,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    host(r'web', 'web.urls', name='web'),
+    host(r'web', 'core.apps.web.urls', name='web'),
     # host(r'api', 'api.urls', name='api'),
     host(r'staff', 'core.urls', name='staff'),
     # host(r'dash', 'dash.urls', name='dashboard'),
